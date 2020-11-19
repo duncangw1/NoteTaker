72e8395664cdae29a0a6e0d1257bd82f5862172b
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-11-19 17:27:50 -0500

    Created more apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,47 @@
-// Load Data
-var notesData = require("../db/store");
+// Dependencies
+var fs = require("fs");
 
 // Routing
 module.exports = function (app) {
-  // API GET Requests
-  app.get("/api/notes", function (req, res) {
-    res.json(notesData);
-  });
+  // Reading notes
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) {
+      throw err;
+    }
+    // Setting notes to variable notesData
+    var notesData = JSON.parse(data);
 
-  // API POST Requests
-  app.post("/api/notes", function (req, res) {
-    notesData.push(req.body);
-    res.json(true);
+    // API GET Requests
+    app.get("/api/notes", function (req, res) {
+      res.json(notesData);
+    });
+
+    app.get("/api/notes/:id", function (req, res) {
+      res.json(notesData[req.params.id]);
+    });
+
+    // API POST Requests
+    app.post("/api/notes", function (req, res) {
+      notesData.push(req.body);
+      res.json(true);
+    });
+
+    // API note deletion by ID
+    app.delete("/api/notes/:id", function (req, res) {
+      notesData.splice(req.params.id, 1);
+      updateDbJson();
+    });
+
+    // Function to update the db.json file
+    function updateDbJson() {
+      fs.writeFile("db/db.json", JSON.stringify(notesData, "\t"), function (
+        err
+      ) {
+        if (err) {
+          throw err;
+        }
+        return true;
+      });
+    }
   });
 };
